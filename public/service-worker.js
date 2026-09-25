4355f09f862c682a6898aad14871cc56de03eee9
75Atahan <ozer@luna.com.tr>
2026-09-26 00:22:39 +0300

    Update CACHE_NAME to 'lso-v10'

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'lso-v7';
+const CACHE_NAME = 'lso-v10';
 const APP_SHELL = ['/', '/index.html', '/styles.css', '/app.js', '/manifest.json', '/icon.svg'];
 
 self.addEventListener('install', (event) => {
